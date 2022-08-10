dc37699c3b9fc89413169cfe3a5ab56b279a1ee8
Vinícius Lins <viniciusaclins@gmail.com>
2022-08-10 20:12:19 -0300

    feat: :sparkles: Added the grid into the Project
    Was created the css grid into the projeto

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,9 +1,21 @@
 <template>
-
+    <div id="grid">
+    </div>
 </template>
 
 <script>
 </script>
 
-<style>
+<style scoped>
+#grid {
+    display: grid;
+    height: 100vh;
+    grid-template-columns: 71px 240px auto 240px;
+    grid-template-rows: 46px auto 52px;
+    grid-template-areas: 
+        "SL SN CI CI"
+        "SL CL CD UL"
+        "SL UI CD UL"
+    ;
+}
 </style>
